681b311378d6c99ade410d6d893015e0ce2870d8
SanskarPande32 <sanskarpande3@gmail.com>
2026-08-17 00:36:22 +0530

    Fix API client import and routes

--- frontend/src/api/platform.js
+++ frontend/src/api/platform.js
@@ -1,3 +1,5 @@
+import { http } from './http';
+
 export const platformApi = {
   health: () => http.get('/api/health').then(({ data }) => data),
   weather: () => http.get('/api/weather').then(({ data }) => data),
@@ -7,12 +9,14 @@ export const platformApi = {
   route: (id) => http.get(`/routes/${id}`).then(({ data }) => data),
 
   waterPoints: () => http.get('/waterpoints').then(({ data }) => data),
-  waterPointsByStop: (stopId) => http.get(`/waterpoints/stop/${stopId}`).then(({ data }) => data),
+  waterPointsByStop: (stopId) =>
+    http.get(`/waterpoints/stop/${stopId}`).then(({ data }) => data),
 
   events: () => http.get('/events').then(({ data }) => data),
 
   user: (id) => http.get(`/api/users/${id}`).then(({ data }) => data),
   users: () => http.get('/api/users').then(({ data }) => data),
   createUser: (user) => http.post('/api/users', user).then(({ data }) => data),
-  updateUser: (id, user) => http.put(`/api/users/${id}`, user).then(({ data }) => data),
+  updateUser: (id, user) =>
+    http.put(`/api/users/${id}`, user).then(({ data }) => data),
 };
